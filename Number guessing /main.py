b66e173e4ae7cbb4fee77675e5ebde8f91169944
Bhavna Bhatt <135252211+bbhatt26@users.noreply.github.com>
2026-06-21 11:32:23 +0530

    Add number guessing game with easy and hard modes
    Implement number guessing game with difficulty levels.

---
+++ Number guessing /main.py
@@ -0,0 +1,36 @@
+import random
+
+ask = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+num = random.randint(1,10)
+
+def level_of_game(ask):
+    if ask == 'easy':
+        return 10
+    elif ask == 'hard':
+        return 5
+    else:
+        return "Invalid input! Type 'easy' or 'hard': "
+
+    
+def game(lives,num):
+    while lives>0:
+        print(f"You've {lives} lives left ")
+        guess = int(input("Guess a number between 1-10: "))
+        
+        if guess == num:
+            print("You guessed it right, its", num)
+            break
+        elif guess> num:
+            print("Too high")
+            lives-=1
+        elif guess<num:
+            print("Too low")
+            lives-=1
+            
+lives= level_of_game(ask)
+if lives==0:
+    print("You lose")
+else:
+    game(lives,num)
+
+
